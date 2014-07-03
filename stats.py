1523b18830cc9ab13469a960634938668363ef15
Jatin Shah <jatindshah@gmail.com>
2014-07-03 13:13:21 +0530

    Compute statistics - Function to compute category statistics in aggregate

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 import gc
 
+
 def compute_integer_stats(input_file, chunk_size):
     """
     Compute training data statistics
@@ -82,11 +83,61 @@ def compute_category_stats(input_file, category_label, chunk_size):
         frame['count'] = stats_category.groupby("category").sum().values
         stats_category = frame
 
+        # Force garbage collection
         gc.collect()
 
         count += 1
 
     return stats_category.describe()
+
+
+def compute_category_stats_all(input_file, chunk_size):
+    """
+    Compute training data statistics
+    Categorical Variables: Number of categories, Histogram
+    Integer Variables: Min, Max, Mean
+    Click: Click, Not Click
+
+    :return:
+    """
+    stats_category = {}
+    for i in range(1, 27):
+        stats_category['C' + str(i)] = pd.DataFrame()
+
+    reader = pd.read_csv(input_file, chunksize=chunk_size)
+
+    count = 0
+    for chunk in reader:
+        print 'Reading line:' + str(count * chunk_size)
+
+        chunk_category = chunk.iloc[:, 15:]
+
+        for i in range(1, 27):
+            category_label = 'C' + str(i)
+
+            frame = pd.DataFrame()
+            frame['category'] = chunk_category.groupby(category_label).size().index
+            frame['count'] = chunk_category.groupby(category_label).size().values
+            stats_category[category_label] = pd.concat([stats_category[category_label], frame])
+
+            # Aggregate on common category values
+            frame = pd.DataFrame()
+            frame['category'] = stats_category[category_label].groupby('category').sum().index
+            frame['count'] = stats_category[category_label].groupby("category").sum().values
+            stats_category[category_label] = frame
+
+            gc.collect()
+
+        count += 1
+
+    stats_category_agg = pd.DataFrame()
+    for i in range(1, 27):
+        frame = stats_category['C' + str(i)].groupby('category').sum().describe().transpose()
+        frame.reset_index()
+        frame.index = ['C' + str(i)]
+        stats_category_agg = pd.concat([stats_category_agg, frame])
+
+    print stats_category_agg
 
 
 def main():
@@ -99,15 +150,16 @@ def main():
         elif action_type == 'category':
             input_file = sys.argv[2]
             chunk_size = int(sys.argv[3])
-            stats_category = pd.DataFrame()
-            for i in range(1, 27):
-                category_label = 'C' + str(i)
-                print "Starting analysis for category: " + category_label
-                frame = compute_category_stats(input_file, category_label, chunk_size).transpose()
-                frame.reset_index()
-                frame.index = [category_label]
-                stats_category = pd.concat([stats_category, frame])
-            print stats_category
+            # stats_category = pd.DataFrame()
+            # for i in range(1, 27):
+            # category_label = 'C' + str(i)
+            #     print "Starting analysis for category: " + category_label
+            #     frame = compute_category_stats(input_file, category_label, chunk_size).transpose()
+            #     frame.reset_index()
+            #     frame.index = [category_label]
+            #     stats_category = pd.concat([stats_category, frame])
+            # print stats_category
+            compute_category_stats_all(input_file, chunk_size)
     else:
         print "Usage: python stats.py integer <input file> <chunk size>"
         print "Usage: python stats.py category <input file> <chunk size>"
