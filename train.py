12baa12f51aa2e25a44889bed9001cb335636024
Jatin Shah <jatindshah@gmail.com>
2014-07-10 10:12:52 +0530

    Integer feature selection

--- train.py
+++ train.py
@@ -4,22 +4,19 @@ import sklearn.feature_extraction
 import sklearn.linear_model
 import sklearn.metrics
 import gc
+import sys
 
 
 integer_stats_file = 'data/integer_stats.csv'
 category_stats_file = 'data/category_stats.csv'
 
 train_file_prefix = 'train_split'
-train_file = range(0, 459)
-# train_file = range(10)
+# train_file = range(0, 459)
+train_file = range(1, 20)
 cv_file = range(0, 1)
 test_file_prefix = 'test_split'
-test_file = range(61)
-# test_file = range(10)
-
-integer_features = ['I' + str(i) for i in range(1, 14)]
-category_features = ['C' + str(i) for i in range(1, 27)]
-features = integer_features + category_features
+# test_file = range(61)
+test_file = range(10)
 
 
 def transform(input_file, keep_features, stats, file_type='train'):
@@ -27,6 +24,8 @@ def transform(input_file, keep_features, stats, file_type='train'):
 
     data_id = data['Id']
 
+    keep_integer_features = [x for x in keep_features if x.startswith("I")]
+    keep_category_features = [x for x in keep_features if x.startswith("C")]
     if file_type == 'train':
         data_integer = data.iloc[:, 2:15]
         data_category = data.iloc[:, 15:]
@@ -37,24 +36,36 @@ def transform(input_file, keep_features, stats, file_type='train'):
     # Set minimum value of features to 0
     data_integer['I2'] = data_integer['I2'].apply(lambda x: max(x, 0))
 
+    # Filter out features
+    data_integer = data_integer[keep_integer_features]
+    data_category = data_category[keep_category_features]
+    stats['integer'] = stats['integer'].loc[keep_integer_features, :]
+    stats['category'] = stats['category'].loc[keep_category_features, :]
+
+
     # Mean normalization
     data_feature_integer = (data_integer.values.astype('float') - stats['integer']['mean'].values) / stats['integer'][
         'std'].values
 
     # Replace NaN with mean value
-    # X_train_integer = np.isnan(X_train_integer) * stats['integer']['mean'].values + \
-    # (~np.isnan(X_train_integer)) * X_train_integer
+    # data_feature_integer = np.isnan(data_feature_integer) * stats['integer']['mean'].values + \
+    # (~np.isnan(data_feature_integer)) * data_feature_integer
 
     # Truncate large integer values to 5x std. dev
-    data_feature_integer = np.minimum(data_feature_integer, 5 * stats['integer']['std'].values)
+    data_feature_integer = np.minimum(data_feature_integer, 2 * stats['integer']['std'].values)
 
-    # f^2 (TBD) & log(1+f) features
+
+    # log(1+f) integer features
+    data_integer_log_features = np.log(1 + data_feature_integer)
+
+    # combine features
     data_feature_integer = np.hstack([np.ones((data_feature_integer.shape[0], 1)),
-                                      data_feature_integer, np.log(1 + data_feature_integer)])
+                                      data_feature_integer, data_integer_log_features])
 
     data_feature_integer = [dict(('I' + str(j), u)
                                  for j, u in enumerate(item) if str(u) != 'nan')
                             for item in data_feature_integer]
+
 
     # Categorical features
     data_feature_category = [dict((u, 1) for u in item if str(u) != 'nan') for item in data_category.values]
@@ -77,50 +88,64 @@ def transform(input_file, keep_features, stats, file_type='train'):
 def main():
     # Load statistics
     stats = {'integer': pd.read_csv(integer_stats_file), 'category': pd.read_csv(category_stats_file)}
-
-    clf = sklearn.linear_model.SGDClassifier(loss='log')
+    stats['integer'].index = stats['integer'].iloc[:, 0]
+    stats['category'].index = stats['category'].iloc[:, 0]
 
     all_classes = np.array([0, 1])
 
+    integer_features = ['I' + str(i) for i in range(1, 14)]
+    integer_features = [v for v in integer_features
+                        # if v not in ['I3', 'I1', 'I12', 'I2', 'I7', 'I11', 'I4', 'I5', 'I9', 'I13', 'I10']]
+                        if v not in ['I3', 'I1', 'I12', 'I2', 'I7']]
+
+    category_features = ['C' + str(i) for i in range(1, 27)]
+    # category_features = [v for v in category_features if v not in []]
+
+    # f = sys.argv[1]
+    # category_features.remove(f)
+    # print category_features
+    all_features = integer_features + category_features
+
+
+    # Train all features
+    clf = sklearn.linear_model.SGDClassifier(loss='log')
     for j in train_file:
         train_file_name = 'data/{0}{1}.csv'.format(train_file_prefix, str(j).zfill(3))
-        # train_file_name = 'data/{0}{1}.csv'.format(train_file_prefix, str(j).zfill(2))
-        print 'Training file' + train_file_name
-        X_train, y_train, id_train = transform(train_file_name, features, stats)
+        print 'Training file ' + train_file_name
+        X_train, y_train, id_train = transform(train_file_name, all_features, stats)
 
         clf.partial_fit(X_train, y_train, classes=all_classes)
 
         # Force garbage collection
         gc.collect()
 
-    # Load CV data
+    # Load & cross-validate data
     for j in cv_file:
         val_file_name = 'data/{0}{1}.csv'.format(train_file_prefix, str(j).zfill(3))
-        # val_file_name = 'data/{0}{1}.csv'.format(train_file_prefix, str(j).zfill(2))
-        X_val, y_val, id_val = transform(val_file_name, features, stats)
+        X_val, y_val, id_val = transform(val_file_name, all_features, stats)
         y_predict = clf.predict_proba(X_val)
 
         print "CV Error: " + str(sklearn.metrics.accuracy_score(y_val.values, y_predict.argmax(axis=1)))
         print "CV Log Loss: " + str(sklearn.metrics.log_loss(y_val.values, y_predict))
 
-    # Predict test data
-    with open('test_pred.csv', 'wb+') as f:
-        f.write('Id,Predicted\n')
-        for j in test_file:
-            test_file_name = 'data/{0}{1}.csv'.format(test_file_prefix, str(j).zfill(2))
-            print 'Predicting file' + test_file_name
-
-            X_test, id_test = transform(test_file_name, features, stats, file_type='test')
-            y_test_predict = clf.predict_proba(X_test)
-
-            # Probability of a click
-            y_test_prob = y_test_predict[:, 1]
-            y_out = np.vstack([id_test, y_test_prob]).transpose()
-
-            np.savetxt(f, y_out, delimiter=",", fmt=['%d', '%.4f'])
-
-            # Garbage collection
-            gc.collect()
+        # # Predict test data
+        # with open('test_pred.csv', 'wb+') as f:
+        # f.write('Id,Predicted\n')
+        #     for j in test_file:
+        #         test_file_name = 'data/{0}{1}.csv'.format(test_file_prefix, str(j).zfill(2))
+        #         print 'Predicting file' + test_file_name
+        #
+        #         X_test, id_test = transform(test_file_name, features, stats, file_type='test')
+        #         y_test_predict = clf.predict_proba(X_test)
+        #
+        #         # Probability of a click
+        #         y_test_prob = y_test_predict[:, 1]
+        #         y_out = np.vstack([id_test, y_test_prob]).transpose()
+        #
+        #         np.savetxt(f, y_out, delimiter=",", fmt=['%d', '%.4f'])
+        #
+        #         # Garbage collection
+        #         gc.collect()
 
 
 main()
